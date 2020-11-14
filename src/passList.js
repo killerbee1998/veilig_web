014fea84a30ac00ac0526dca9632cd96cc105468
riasat1998 <riasat1998@gmail.com>
2020-11-14 20:54:07 +0600

    passList: changed error messages to be less ambigious

--- src/passList.js
+++ src/passList.js
@@ -1,6 +1,17 @@
+const ROOT_URL = 'https://mighty-anchorage-78746.herokuapp.com/'
+
 let data = JSON.parse(localStorage.getItem('login_data'))
 let token = data.token
 let key = data.key
+
+console.log(data)
+jQuery( () =>{
+  displayPass(ROOT_URL+'store/displayPass', {token:token, authKey:key})
+  .then( (response) =>{
+    console.log(response)
+  })
+})
+
 
 displayPass = async(url, data) => {
     const response = await fetch(url, {
