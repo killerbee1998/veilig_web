961073f3a482ca4f3580d39b77e06cd804d1d7bc
riasat1998 <riasat1998@gmail.com>
2020-11-14 19:58:31 +0600

    passList: created a func to request user passwords

--- src/passList.js
+++ src/passList.js
@@ -1,3 +1,20 @@
 let data = JSON.parse(localStorage.getItem('login_data'))
 let token = data.token
 let key = data.key
+
+displayPass = async(url, data) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors', 
+      cache: 'no-cache', 
+      credentials: 'same-origin', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data) 
+    });
+    return response.json(); 
+    
+  } 
